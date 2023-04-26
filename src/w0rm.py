b1c5580e6de9f8e1ae0d22c7ad79cff812969648
Francesco Favale <francesco@favale.eu>
2023-04-26 15:47:37 +0200

    w0rm begun

---
+++ src/w0rm.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+"""
+A simple program meant to preprocess the training video data into images
+"""
+
+
+def main() -> None:
+    """w0rm main function"""
+    return
+
+
+if __name__ == "__main__":
+    main()
